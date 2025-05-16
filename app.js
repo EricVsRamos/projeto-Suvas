22f3e89cd95b6cb6afc876442a660e359ecde198
Eric Ramos <EricVsRamos>
2025-05-16 13:15:08 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var classname = document.body.classname;
+    if(classname == "light-theme") {
+        this.textcontent = "dark";
+    }
+    else {
+        this.textcontent ="light";
+    }
+    console.log('curret class name:' + classname);
+
+});
